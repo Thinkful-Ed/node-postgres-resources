cad5d49821fc2cd50c01f64243dd3d82eeaab0c8
Ariana Faraday <ariana@thinkful.com>
2019-01-04 11:49:06 -0500

    adds intro to node and intro to express resources

---
+++ book.js
@@ -0,0 +1,266 @@
+module.exports = [
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Dean R Koontz",
+      description:
+        "Odd Thomas, who can communicate with the dead, confronts evil forces in a California coastal town.",
+      price: 27,
+      publisher: "Bantam",
+      title: "ODD HOURS",
+      rank: 1,
+      rank_last_week: 0,
+      weeks_on_list: 1,
+      id: "5b4aa4ead3089013507db18b"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Stephenie Meyer",
+      description:
+        "Aliens have taken control of the minds and bodies of most humans, but one woman won’t surrender.",
+      publisher: "Little, Brown",
+      title: "THE HOST",
+      rank: 2,
+      rank_last_week: 1,
+      weeks_on_list: 3,
+      id: "5b4aa4ead3089013507db18c"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Emily Giffin",
+      description:
+        "A woman's happy marriage is shaken when she encounters an old boyfriend.",
+      publisher: "St. Martin's",
+      title: "LOVE THE ONE YOU'RE WITH",
+      rank: 3,
+      rank_last_week: 2,
+      weeks_on_list: 2,
+      id: "5b4aa4ead3089013507db18d"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Patricia Cornwell",
+      description:
+        'A Massachusetts state investigator and his team from "At Risk" confront a rogue association of municipal police departments.',
+      publisher: "Putnam",
+      title: "THE FRONT",
+      rank: 4,
+      rank_last_week: 0,
+      weeks_on_list: 1,
+      id: "5b4aa4ead3089013507db18e"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Chuck Palahniuk",
+      description:
+        "An aging porn queens aims to cap her career by having sex on film with 600 men in one day.",
+      publisher: "Doubleday",
+      title: "SNUFF",
+      rank: 5,
+      rank_last_week: 0,
+      weeks_on_list: 1,
+      id: "5b4aa4ead3089013507db18f"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "James Patterson and Gabrielle Charbonnet",
+      description: "A woman finds an unexpected love",
+      publisher: "Little, Brown",
+      title: "SUNDAYS AT TIFFANY’S",
+      rank: 6,
+      rank_last_week: 3,
+      weeks_on_list: 4,
+      id: "5b4aa4ead3089013507db190"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "John Sandford",
+      description:
+        "The Minneapolis detective Lucas Davenport investigates a string of murders of young Goths.",
+      publisher: "Putnam",
+      title: "PHANTOM PREY",
+      rank: 7,
+      rank_last_week: 4,
+      weeks_on_list: 3,
+      id: "5b4aa4ead3089013507db191"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Jimmy Buffett",
+      description:
+        "A Southern family tries to hide its pet pig at a New York luxury hotel.",
+      publisher: "Little, Brown",
+      title: "SWINE NOT?",
+      rank: 8,
+      rank_last_week: 6,
+      weeks_on_list: 2,
+      id: "5b4aa4ead3089013507db192"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Elizabeth George",
+      description:
+        "In Cornwall, trying to recover from his wife's death, Detective Thomas Lynley becomes involved in a murder investigation.",
+      publisher: "Harper",
+      title: "CARELESS IN RED",
+      rank: 9,
+      rank_last_week: 8,
+      weeks_on_list: 3,
+      id: "5b4aa4ead3089013507db193"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "David Baldacci",
+      description:
+        "An intelligence agent and a journalist team up against a warmongering defense contractor.",
+      publisher: "Grand Central",
+      title: "THE WHOLE TRUTH",
+      rank: 10,
+      rank_last_week: 7,
+      weeks_on_list: 5,
+      id: "5b4aa4ead3089013507db194"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Troy Denning",
+      description:
+        "The New Jedi order battles Darth Caedus, Darth Vader's gradson; a 'Star Wars: Legacy of the Force' novel.",
+      price: 27,
+      publisher: "Del Rey/Ballantine",
+      title: "INVINCIBLE",
+      rank: 11,
+      rank_last_week: 5,
+      weeks_on_list: 2,
+      id: "5b4aa4ead3089013507db195"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "James Frey",
+      description:
+        'A novel, set in Los Angeles, by the author of "A Million Little Pieces."',
+      publisher: "Harper",
+      title: "BRIGHT SHINY MORNING",
+      rank: 12,
+      rank_last_week: 9,
+      weeks_on_list: 2,
+      id: "5b4aa4ead3089013507db196"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Garth Stein",
+      description:
+        "A Lab-terrier mix with great insight into the human condition helps his owner, a struggling race car driver.",
+      publisher: "Harper",
+      title: "THE ART OF RACING IN THE RAIN",
+      rank: 13,
+      rank_last_week: 0,
+      weeks_on_list: 1,
+      id: "5b4aa4ead3089013507db197"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Debbie Macomber",
+      description:
+        "A widow who owns a bookstore on Blossom Street compiles a list of things she always wanted to do.",
+      publisher: "Mira",
+      title: "TWENTY WISHES",
+      rank: 14,
+      rank_last_week: 10,
+      weeks_on_list: 4,
+      id: "5b4aa4ead3089013507db198"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Jeff Shaara",
+      description: "A novel about the Allied invasion in France in World War II.",
+      price: 28,
+      publisher: "Ballantine",
+      title: "THE STEEL WAVE",
+      rank: 15,
+      rank_last_week: 11,
+      weeks_on_list: 2,
+      id: "5b4aa4ead3089013507db199"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Phillip Margolin",
+      description: "",
+      price: 0,
+      publisher: "HarperCollins Publishers",
+      title: "EXECUTIVE PRIVILEGE",
+      rank: 16,
+      rank_last_week: 0,
+      weeks_on_list: 0,
+      id: "5b4aa4ead3089013507db19a"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Jhumpa Lahiri",
+      description:
+        "Stories of the anxiety and transformation experienced by Bengalis in America.",
+      price: 0,
+      publisher: "Knopf",
+      title: "UNACCUSTOMED EARTH",
+      rank: 17,
+      rank_last_week: 0,
+      weeks_on_list: 0,
+      id: "5b4aa4ead3089013507db19b"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "Joseph O'Neill",
+      description:
+        "A Dutchman deserted by his wife after 9/11 finds solace in New York’s immigrant cricket scene.",
+      price: 0,
+      publisher: "Knopf Publishing Group",
+      title: "NETHERLAND",
+      rank: 18,
+      rank_last_week: 0,
+      weeks_on_list: 0,
+      id: "5b4aa4ead3089013507db19c"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "John Grisham",
+      description:
+        "Political and legal intrigue ensue when a Mississippi court rules against a company accused of dumping toxic waste.",
+      price: 0,
+      publisher: "Doubleday Publishing",
+      title: "THE APPEAL",
+      rank: 19,
+      rank_last_week: 0,
+      weeks_on_list: 0,
+      id: "5b4aa4ead3089013507db19d"
+    },
+    {
+      bestsellers_date: 1211587200000,
+      published_date: 1212883200000,
+      author: "James Rollins",
+      description: "",
+      price: 0,
+      publisher: "Random House Publishing Group",
+      title: "INDIANA JONES AND THE KINGDOM OF THE CRYSTAL SKULL",
+      rank: 20,
+      rank_last_week: 0,
+      weeks_on_list: 0,
+      id: "5b4aa4ead3089013507db19e"
+    }
+  ];
